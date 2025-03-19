d0fbd3ad4c80586d754f4fe4eb766650579df477
RaihanShakeel <raihanshakeel58@gmail.com>
2025-03-19 10:32:45 +0500

    fixed the error

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,17 +1,18 @@
+//app\query\route.ts
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// export async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data;
-// }
+	return data;
+}
 
 export async function GET() {
   // return Response.json({
